01e10fffc4361a30cb969842c0cc907910e1c786
opusunju moses <opuzmoses@gmail.com>
2022-12-25 00:12:39 +0100

    updating responsive scss codes

---
+++ src/components/home.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Review from "./Review";
+import ServiceSec from "./Services";
+import TeamSec from "./Team";
+import "../Styles/home.scss";
+
+const Home = () => {
+  return (
+    <>
+      <section>
+        <div className="nav">
+          <NavBar />
+        </div>
+
+        <div className="hero">
+          <Hero />
+        </div>
+
+        <div className="padding">
+          <div className="service_sec">
+            <ServiceSec />
+          </div>
+
+          <div className="review_sec">
+            <Review />
+          </div>
+
+          <div className="team_sec">
+            <TeamSec />
+          </div>
+        </div>
+
+        <div className="footer">
+          <Footer />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
